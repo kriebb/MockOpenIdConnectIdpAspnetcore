5ec8b0026758d759b2d6f17d9c18d501038bc039
Kristof Riebbels <github.2023@kriebbels.me>
2024-04-29 20:08:36 +0200

    adding documentation on self signed token

--- src/OIdcMockingInfrastructure/Security/SelfSignedTokenPemCertificateFactory.cs
+++ src/OIdcMockingInfrastructure/Security/SelfSignedTokenPemCertificateFactory.cs
@@ -9,14 +9,24 @@ public static class SelfSignedTokenPemCertificateFactory
     {
         using (RSA rsa = RSA.Create())
         {
-            // Generate a 2048 bit RSA key pair
+            // Creating an RSA key pair:
+            // RSA (Rivest–Shamir–Adleman) is a public-key cryptosystem that is widely used for secure data transmission.
+            // In this class, a 2048-bit RSA key pair is generated. This key pair consists of a public key and a private key.
+            // The public key can be freely distributed, while the private key must be kept secret.
+        
             rsa.KeySize = 2048;
 
-            // Create a new self signed certificate
+            // Creating a self-signed certificate:
+            // A self-signed certificate is a certificate that is not signed by a trusted certificate authority (CA).
+            // Instead, it is signed with its own private key.
+            // This is done using the CertificateRequest class in .NET, which allows you to create a new certificate request, and then self-sign it. 
             CertificateRequest request = new CertificateRequest("cn=i.do.not.exist", rsa, HashAlgorithmName.SHA256,
                 RSASignaturePadding.Pkcs1);
 
-            // Set the validity period of the certificate
+             
+            // Setting the validity period of the certificate:
+            // The certificate is valid from one day in the past to 3650 days in the future.
+            // This is done to avoid any potential issues with time synchronization.
             request.CertificateExtensions.Add(new X509BasicConstraintsExtension(true, false, 0, true));
             request.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension(new OidCollection
             {
@@ -26,7 +36,9 @@ public static class SelfSignedTokenPemCertificateFactory
             X509Certificate2 cert = request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)),
                 new DateTimeOffset(DateTime.UtcNow.AddDays(3650)));
 
-            // Export the certificate to a PEM file
+            // Creating a PemCertificate object: This object contains the PEM-formatted certificate and keys. It is returned by the Create method.
+            // PEM (Privacy Enhanced Mail) is a widely used format for storing and transmitting cryptographic keys and certificates.
+
             byte[] certificateBytes = cert.RawData;
             char[] certificatePem = PemEncoding.Write("CERTIFICATE", certificateBytes);
 
@@ -37,7 +49,13 @@ public static class SelfSignedTokenPemCertificateFactory
             byte[] privKeyBytes = key.ExportPkcs8PrivateKey();
             char[] pubKeyPem = PemEncoding.Write("PUBLIC KEY", pubKeyBytes);
             char[] privKeyPem = PemEncoding.Write("PRIVATE KEY", privKeyBytes);
-
+            //This support is particularly useful in scenarios where using X509 certificates can be challenging, such as on a build server.
+            //When using X509 certificates, the certificate is often loaded into a user profile's certificate store.
+             //One of these limitations is that it requires a user profile to be loaded.
+            //PEM files do not require a user profile or access to a certificate store.
+            
+            //In ASP.NET Core, you can use the X509Certificate2 class to load a certificate from a PEM file.
+            //This can be done entirely in memory, without the need for a user profile or access to a certificate store.
 
             var pemCertificate = new PemCertificate(
                 Certificate: new string(certificatePem),
